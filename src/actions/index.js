06c39bd36fdfd4fda25255f1b188ebde98f1a674
Tyler <laorik1@live.com>
2019-10-24 19:38:51 -0400

    added ability to add custom filter terms to the app

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,13 @@ import axios from "axios";
 import * as R from "ramda";
 
 const HERE_API_URL = "https://geocoder.api.here.com/6.2/geocode.json";
-const APP_CODE = "XiAH-_8pbmtvUn1IKaHZ2w";
-const API_KEY = "69zZHOBAGOl9MV8qbVmG";
+const HERE_APP_CODE = "XiAH-_8pbmtvUn1IKaHZ2w";
+const HERE_API_KEY = "69zZHOBAGOl9MV8qbVmG";
+
+const MEETUP_API_URL = "https://api.meetup.com/find/upcoming_events";
+const MEETUP_API_KEY = "597535717d26481d695a15d7f2e753f";
+
+//ramda object path to lat/lon cordinates
 const RESPONSE_RAMDA_PATH = [
   "data",
   "Response",
@@ -16,21 +21,21 @@ const RESPONSE_RAMDA_PATH = [
 ];
 
 export const TOGGLE_FILTER = "toggle_filter";
-
 export const FETCH_EVENTS = "fetch_events";
+export const ADD_FILTER_TERM = "add_term";
 
 export function fetchEvents(search) {
+  //start and axios request to HERE maps, then with that response (which translates address into lat/lon)
+  //request from Meetup upcoming events using the lat/lon
   const request = axios
     .get(
-      `${HERE_API_URL}?app_id=${API_KEY}&app_code=${APP_CODE}&searchtext=${search}`
+      `${HERE_API_URL}?app_id=${HERE_API_KEY}&app_code=${HERE_APP_CODE}&searchtext=${search}`
     )
     .then(function(response) {
       let lat = R.pathOr(null, [...RESPONSE_RAMDA_PATH, "Latitude"], response);
       let lon = R.pathOr(null, [...RESPONSE_RAMDA_PATH, "Longitude"], response);
-      console.log("lat: ", lat);
-      console.log("lon: ", lon);
       return axios.get(
-        `https://api.meetup.com/find/upcoming_events?key=597535717d26481d695a15d7f2e753f&lat=${lat}&lon=${lon}&sign=true&page=100`
+        `${MEETUP_API_URL}?key=${MEETUP_API_KEY}&lat=${lat}&lon=${lon}&sign=true&page=250`
       );
     })
     .catch(function(error) {
@@ -43,9 +48,18 @@ export function fetchEvents(search) {
   };
 }
 
+//toggle a filters state
 export function toggleFilter(filter) {
   return {
     type: TOGGLE_FILTER,
+    payload: filter
+  };
+}
+
+//add a custom filter term
+export function addFilterTerm(filter) {
+  return {
+    type: ADD_FILTER_TERM,
     payload: filter
   };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import EventsReducer from "./reducer-search";
 import FiltersReducer from "./filter-reducers";
+import CustomFilterReducer from "./custom-filter-reducers";
 
 const rootReducer = combineReducers({
   events: EventsReducer,
   form: formReducer,
-  filters: FiltersReducer
+  filters: FiltersReducer,
+  customFilters: CustomFilterReducer
 });
 
 export default rootReducer;
