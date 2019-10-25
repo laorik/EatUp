f0c118c8e8b90df0a976d649f984da7fe389f3d5
MrWhatleyUNC <chris.whatley87@gmail.com>
2019-10-25 12:03:34 -0400

    Some Styling

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -4,13 +4,14 @@ import {Link} from 'react-router-dom';
 
 const SearchResult = props => {
     return (
-        <li className="list-group-item" key={props.event.id}>
-            <Link to={`/event/${props.event.id}`}>
-            {props.event.name}</Link><br></br>
-            Date: {moment(props.event.local_date).format('MMMM Do YYYY')}<br></br>
-            Time: {moment(props.event.local_time,'HH:mm:ss').format('h:mm:ss A')}
-            
-        </li>
+        <div className= "background">
+            <li className="list-group-item" key={props.event.id}>
+                <Link to={`/event/${props.event.id}`}>
+                {props.event.name}</Link><br></br>
+                Date: {moment(props.event.local_date).format('MMMM Do YYYY')}<br></br>
+                Time: {moment(props.event.local_time,'HH:mm:ss').format('h:mm:ss A')} 
+            </li>
+        </div>
     )
 }
 
--- src/containers/SearchView.js
+++ src/containers/SearchView.js
@@ -3,14 +3,29 @@ import React, { Component } from "react";
 import SearchBar from "../components/SearchBar";
 import FilterTerms from "../components/FilterTerms";
 import SearchResultsList from "../components/SearchResultsList";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class SearchView extends Component {
   render() {
     return (
       <div className="search-view">
-        <SearchBar />
-        <FilterTerms />
-        <SearchResultsList />
+        <Container>
+          <Row>
+            <Col></Col>
+            <Col>
+              <SearchBar />
+              <FilterTerms />
+            </Col>
+            <Col></Col>
+          </Row>
+          <Row>
+            <Col>
+              <SearchResultsList />
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
